41d67587e2e41812b86d7af82641c3dcf57c969a
riley soloner <rileysoloner@gmail.com>
2022-09-26 11:07:39 -0400

    added collapsibles to main story points

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,46 @@
 import React, { useState } from 'react'
+import useCollapse from 'react-collapsed'
+
+const Collapsible = () => {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
+  return ( <div>
+    <button {...getToggleProps()} >{isExpanded ? 'collapse' : 'expand'} </button>
+    <div {...getCollapseProps()} >
+      <div className="content">
+        <label htmlfor="contextInput"
+          className="innerContextLabel">Context</label> <br></br>
+        <input name="contextInput"
+          placeholder='your text here'></input> <br /><br />
+        <label htmlfor="catalystInput"
+          className="innerCatalystLabel">Catalyst</label> <br></br>
+        <input name="catalystInput"
+          placeholder='your text here'></input> <br /><br />
+        <label htmlfor="pointOfNoReturnInput"
+          className="innerPointOfNoReturnLabel" >Point of No Return</label> <br></br>
+        <input name="pointOfNoReturnInput"
+          placeholder='your text here'></input> <br /><br />
+        <label htmlfor="climaxInput"
+          className="innerClimaxLabel" >Climax</label> <br></br>
+        <input name="climaxInput"
+          placeholder='your text here'></input> <br /><br />
+        <label htmlfor="denouementInput"
+          className="innerDenouementLabel" >Denouement</label> <br></br>
+        <input name="denouementInput"
+          placeholder='your text here'></input> <br /><br />
+      </div>
+    </div> <br></br>
+  </div>
+  )
+
+}
 
 const Form = () => {
   const [isShown, setIsShown] = useState(false)
-  const [isExpanded, setIsExpanded] = useState(true)
+  // const [isExpanded, setIsExpanded] = useState(true)
 
-  const toggleExpanded = () => {
-    setIsExpanded((isExpanded) => !isExpanded)
-  }
+  // const toggleExpanded = () => {
+  //   setIsExpanded((isExpanded) => !isExpanded)
+  // }
   return (
 <div>
   <h1>Story Outliner</h1>
@@ -18,17 +52,23 @@ const Form = () => {
            className="contextLabel"
            onMouseEnter={() => setIsShown(true)}
            onMouseLeave={() => setIsShown(false)}>
-            Context
-            </label> <br />
-          {isShown && <div className='contextDefinition'><p>The Introduction, <br /> The Exposition, <br />
-            The Status Quo, <br /> Ordinary World, <br />
-            The Protagonist's Comfort Zone, <br />
-            The Set-Up, <br /> The Hook </p> </div>}
+           Context
+           </label> <br />
+           {isShown && <div className='contextDefinition'>
+           <p>The Introduction, <br />
+              The Exposition, <br />
+              The Status Quo, <br />
+              Ordinary World, <br />
+              Protagonist's Comfort Zone, <br />
+              The Set-Up, <br />
+              The Hook </p>
+            </div>}
     <input type="text"
            name="context-input"
            placeholder="your text here">
     </input> <br />
-    <button onClick={ toggleExpanded }> { isExpanded ? "v" : ">"} </button> <br />
+      <Collapsible /> <br></br>
+
     <label htmlFor="context-input"
            className="catalystLabel">Catalyst
     </label> <br />
@@ -36,7 +76,7 @@ const Form = () => {
            name="catalyst-input"
            placeholder="your text here">
     </input> <br />
-    <button>v</button> <br />
+          <Collapsible /> <br></br>
     <label htmlFor="context-input"
            className="pointOfNoReturnLabel" >Point of No Return
     </label> <br />
@@ -44,7 +84,7 @@ const Form = () => {
            name="point-of-no-return-input"
            placeholder="your text here">
     </input> <br />
-    <button>v</button> <br />
+          <Collapsible /> <br></br>
     <label htmlFor="context-input"
            className="climaxLabel" >Climax
     </label> <br />
@@ -52,7 +92,7 @@ const Form = () => {
            name="climax-input"
            placeholder="your text here">
     </input> <br />
-    <button>v</button> <br />
+          <Collapsible /> <br></br>
     <label htmlFor="context-input"
            className="denouementLabel" >Denouement 
     </label> <br />
@@ -60,7 +100,7 @@ const Form = () => {
            name="denouement-input"
            placeholder="your text here">
     </input> <br />
-    <button>v</button> <br />
+          <Collapsible /> <br></br>
   </div>
   </div>
 </div>
