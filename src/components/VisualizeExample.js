4a8f1e57e9c67bcb504a1724924e2033cd6adce6
riley soloner <rileysoloner@gmail.com>
2022-10-03 17:31:09 -0400

    example collapsible created. need to make 5 separate components for now bc whatever

--- src/components/VisualizeExample.js
+++ src/components/VisualizeExample.js
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { Drawer, Button } from 'rsuite';
 import Collapse from 'rsuite/esm/Animation/Collapse';
 import Collapsible from './Collapsible';
+import ExampleCollapsibleContext from './ExampleCollapsibleContext';
 import VisualizerCollapsible from './VisualizerCollapsible';
-import VisualizeExample from './VisualizeExample';
 
-const Visualize = ({ context = "", catalyst = "", pointOfNoReturn = "", climax = "", denouement = "", innerContext, innerCatalyst, innerPoint, innerClimax, innerDenouement, title=""} ) => {
+const VisualizeExample = () => {
 
   const [open, setOpen] = useState(false);
   const [placement, setPlacement] = useState();
@@ -23,38 +23,35 @@ const Visualize = ({ context = "", catalyst = "", pointOfNoReturn = "", climax =
               size={'full'}
               onClose={() => setOpen(false)}>
         <Drawer.Header>
-          <h2>{title ? title : "My Outline" }</h2>
-          <VisualizeExample />
+          <h2>Star Wars (1977) </h2>
         </Drawer.Header>
         <Drawer.Body>
         <div id='visualizerDrawer'>
           <div id="bubbles">
           <div id='contextBubble'>
             <h5>Context:</h5>
-            <p> { context } </p>
-                <VisualizerCollapsible inner={innerContext} /> 
+                <p> A narrow escape in space </p>
+                <ExampleCollapsibleContext /> 
           </div>
           <div id='catalystBubble'>
             <h5>Catalyst:</h5>
-            <p> { catalyst } </p>
-                <VisualizerCollapsible inner={innerCatalyst}
-                />
+                <p> A purchase leads to adventure </p>
+                {/* <ExampleCollapsible /> */}
           </div>
           <div id='pointOfNoReturnBubble'>
             <h5>Point of No Return:</h5>
-            <p> { pointOfNoReturn } </p>
-                <VisualizerCollapsible inner={innerPoint}
-                />
+            <p> Belly of the beast </p>
+                {/* <ExampleCollapsible /> */}
           </div>
           <div id='climaxBubble'>
             <h5>Climax:</h5>
-              <p>{ climax }</p>
-                <VisualizerCollapsible inner={innerClimax} />
+              <p> Dogfight </p>
+                {/* <ExampleCollapsible /> */}
           </div>
           <div id='denouementBubble'>
             <h5>Denouement:</h5>
-              <p> {denouement} </p>
-                <VisualizerCollapsible inner={innerDenouement} />
+              <p> Big Ceremony </p>
+                {/* <ExampleCollapsible /> */}
               </div>
           </div>
             <img id="blankArc" src='./blankArcDarkGreyBg.png' alt="blank arc"></img>
@@ -62,9 +59,9 @@ const Visualize = ({ context = "", catalyst = "", pointOfNoReturn = "", climax =
         </Drawer.Body>
       </Drawer>
       
-      <Button appearance="subtle" color="blue" onClick={() => handleOpen('right')}>Visualize</Button>
+      <Button appearance="subtle" color="red" onClick={() => handleOpen('bottom')}>show me an example</Button>
     </div>
   )
 }
 
-export default Visualize;
+export default VisualizeExample;
